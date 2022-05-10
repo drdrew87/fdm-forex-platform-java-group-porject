e57e339e1a64acb2c8b9f9d04c4ed7f9f072a2cd
andrew yu <andrew.yu@fdmgroup.com>
2022-05-10 22:06:16 +0800

    project creation initial commit

---
+++ project/src/main/java/com/fdm/project/ForexPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.fdm.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForexPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForexPlatformApplication.class, args);
+	}
+
+}
---
+++ project/src/test/java/com/fdm/project/ForexPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fdm.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForexPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
