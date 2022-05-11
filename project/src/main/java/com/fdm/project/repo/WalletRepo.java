ff520311c1e502652e5a20ec4e7bd710ace111f6
andrew yu <andrew.yu@fdmgroup.com>
2022-05-11 14:51:27 +0800

    WalletRepo created

---
+++ project/src/main/java/com/fdm/project/repo/WalletRepo.java
@@ -0,0 +1,16 @@
+package com.fdm.project.repo;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.fdm.project.model.User;
+import com.fdm.project.model.Wallet;
+
+@Repository
+public interface WalletRepo extends JpaRepository<Wallet, Integer>{
+    List<Wallet> getByUser(User user);
+    Optional<List<Wallet>> findByUser(User user);
+}
