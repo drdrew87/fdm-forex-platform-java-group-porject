9d66fe226c98c764d25e98567ab6c6f90e629556
andrew yu <andrew.yu@fdmgroup.com>
2022-05-11 10:49:36 +0800

    Bank model created

---
+++ project/src/main/java/com/fdm/project/model/Bank.java
@@ -0,0 +1,64 @@
+package com.fdm.project.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+
+import org.springframework.stereotype.Component;
+
+@Component
+@Entity
+public class Bank {
+    @Id
+    @SequenceGenerator(name = "BANK_SEQ_GNTR", sequenceName = "BANK_SEQ")
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "BANK_SEQ_GNTR")
+    @Column(name = "bank_account_id")
+    private int bankAccountId;
+    
+    private long bankAccountNumber;
+    
+    private double balance;
+    
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "user_id")
+    private User user;
+
+    public Bank() {
+	super();
+    }
+
+    public long getBankAccountNumber() {
+        return bankAccountNumber;
+    }
+
+    public void setBankAccountNumber(long bankAccountNumber) {
+        this.bankAccountNumber = bankAccountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public int getBankAccountId() {
+        return bankAccountId;
+    }
+    
+    
+}
