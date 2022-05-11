b6735c1396d468f296524b4716cc5acbeb02018c
andrew yu <andrew.yu@fdmgroup.com>
2022-05-11 15:17:46 +0800

    SpotOrderForexRepo created

---
+++ project/src/main/java/com/fdm/project/repo/SpotOrderForexRepo.java
@@ -0,0 +1,14 @@
+package com.fdm.project.repo;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fdm.project.model.SpotOrderForex;
+import com.fdm.project.model.User;
+
+public interface SpotOrderForexRepo extends JpaRepository<SpotOrderForex, Integer>{
+    List<SpotOrderForex> getByUser(User user);
+    Optional<List<SpotOrderForex>> findByUser(User user);
+}
