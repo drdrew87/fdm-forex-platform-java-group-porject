3931c71ca69110d04b0044a5aeb3c9e738c7e65a
Jean Yap <jean.yap@fdmgroup.com>
2022-05-12 23:38:04 +0800

    added new methods to userservice for user validation

--- project/src/main/java/com/fdm/project/service/UserService.java
+++ project/src/main/java/com/fdm/project/service/UserService.java
@@ -3,16 +3,34 @@ package com.fdm.project.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fdm.project.model.User;
+import com.fdm.project.repo.UserRepo;
+
 @Service
 public class UserService {
-
-//	@Autowired
-//	private UserRepo userRepo;
+	
+	@Autowired
+	private UserRepo userRepo;
 	
 	public boolean verifyUser(String username, String password) {
-		// TODO Auto-generated method stub
+		
+		User user = userRepo.getByUsername(username);
+		
+		if (user.getPassword().equals(password)) {
+			return true;
+		} else {
 		return false;
+		}
 	}
 
-	
+	public boolean verifyUsernameIsUnique(String username) {
+		
+		User user = userRepo.getByUsername(username);
+		if (user == null) {
+			return true;
+		} else {
+		return false;
+		}
+	}
+
 }
