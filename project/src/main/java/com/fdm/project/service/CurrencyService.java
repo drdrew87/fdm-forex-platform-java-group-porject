ece3a18c67d5a9fa024d4d83f8ad153717aa880f
andrew yu <andrew.yu@fdmgroup.com>
2022-05-12 23:14:07 +0800

    CurrencyService created

---
+++ project/src/main/java/com/fdm/project/service/CurrencyService.java
@@ -0,0 +1,23 @@
+package com.fdm.project.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.fdm.project.model.Currency;
+import com.fdm.project.repo.CurrencyRepo;
+
+@Service
+public class CurrencyService {
+    @Autowired
+    private CurrencyRepo currencyRepo;
+    
+    public List<Currency> findAllCurrency() {
+	return currencyRepo.findAll();
+    }
+    
+    public Currency findByCurrencyId(int currencyId) {
+	return currencyRepo.getById(currencyId);
+    }
+}
