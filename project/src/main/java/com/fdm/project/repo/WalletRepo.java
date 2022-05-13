e4399e8d4472241b546154bfce2ba3a0d4e2f614
andrew yu <andrew.yu@fdmgroup.com>
2022-05-13 15:47:19 +0800

    getByUserAndCurrency method implemented

--- project/src/main/java/com/fdm/project/repo/WalletRepo.java
+++ project/src/main/java/com/fdm/project/repo/WalletRepo.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.fdm.project.model.Currency;
 import com.fdm.project.model.User;
 import com.fdm.project.model.Wallet;
 
@@ -13,4 +14,6 @@ import com.fdm.project.model.Wallet;
 public interface WalletRepo extends JpaRepository<Wallet, Integer>{
     List<Wallet> getByUser(User user);
     Optional<List<Wallet>> findByUser(User user);
+    
+    Wallet getByUserAndCurrency(User user, Currency currency);
 }
