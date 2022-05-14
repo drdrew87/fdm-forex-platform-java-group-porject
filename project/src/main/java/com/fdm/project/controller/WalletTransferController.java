c2cb3518ae84495b6e33152195afaa4d95a16344
andrew yu <andrew.yu@fdmgroup.com>
2022-05-14 22:42:18 +0800

    wallet transfer paper initial implementation

--- project/src/main/java/com/fdm/project/controller/WalletTransferController.java
+++ project/src/main/java/com/fdm/project/controller/WalletTransferController.java
@@ -1,12 +1,15 @@
 package com.fdm.project.controller;
 
-import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.fdm.project.model.Bank;
 import com.fdm.project.model.User;
@@ -25,20 +28,76 @@ public class WalletTransferController {
     private WalletService walletService;
     
     @GetMapping("/{username}/wallet/transfer")
-    public String accessWalletTransferPage(@PathVariable String username, Model model) {
+    public String accessWalletTransferPage(@PathVariable String username, HttpServletRequest request, Model model) {
 	User currentUser = userService.getUserByUsername(username);
-	List<Bank> bankList = bankService.getBankByUser(currentUser);
-	model.addAttribute("bankList", bankList);
-	if (bankList.size()==0) {
-	    model.addAttribute("emptyBankList", true);
-	}
+	HttpSession session = request.getSession();
 	
-	List<Wallet> walletList = walletService.getWalletByUser(currentUser);
-	model.addAttribute("walletList", walletList);
-	if (walletList.size()==0) {
-	    model.addAttribute("emptyWalletList", true);
+	bankService.displayBankListInDropdown(currentUser, model);
+	
+	boolean isBankSelected = bankService.selectBank(session, model);
+	
+	walletService.displayWalletListInDropdown(currentUser, model);
+
+	boolean isWalletSelected = walletService.selectWallet(session, model);
+	
+	double exchangeRate = 0;
+	if (isBankSelected && isWalletSelected) {
+	    int selectedBankId = Integer.valueOf((String) session.getAttribute("selectedBankId"));
+	    Bank selectedBank = bankService.getBankById(selectedBankId);
+	    int selectedWalletId = Integer.valueOf((String) session.getAttribute("selectedWalletId"));
+	    Wallet selectedWallet = walletService.getWalletById(selectedWalletId);
+	    exchangeRate = selectedWallet.getCurrency().getCurrencyRate() / selectedBank.getCurrency().getCurrencyRate();
 	}
+	model.addAttribute("exchangeRate", exchangeRate);
 	
 	return "wallet_transfer";
+    }
+    
+    @PostMapping("/{username}/wallet/transfer/bankSelection")
+    public String selectBankForTransfer(@PathVariable String username, @RequestParam String selectedBankId, HttpServletRequest request) {
+	HttpSession session = request.getSession();
+	session.setAttribute("selectedBankId", selectedBankId);
+	return "redirect:/" + username + "/wallet/transfer";
+    }
+    
+    @PostMapping("/{username}/wallet/transfer/removeBankSelection")
+    public String removeBankSelection(@PathVariable String username, HttpServletRequest request) {
+	HttpSession session = request.getSession();
+	session.removeAttribute("selectedBankId");
+	return "redirect:/" + username + "/wallet/transfer";
+    }
+    
+    @PostMapping("/{username}/wallet/transfer/walletSelection")
+    public String selectWalletForTransfer(@PathVariable String username, @RequestParam String selectedWalletId, HttpServletRequest request) {
+	HttpSession session = request.getSession();
+	session.setAttribute("selectedWalletId", selectedWalletId);
+	return "redirect:/" + username + "/wallet/transfer";
+    }
+    
+    @PostMapping("/{username}/wallet/transfer/removeWalletSelection")
+    public String removeWalletSelection(@PathVariable String username, HttpServletRequest request) {
+	HttpSession session = request.getSession();
+	session.removeAttribute("selectedWalletId");
+	return "redirect:/" + username + "/wallet/transfer";
+    }
+    
+    @PostMapping("/{username}/wallet/transfer/confirmTransfer")
+    public String confirmTransfer(@PathVariable String username, @RequestParam String transferAmount, HttpServletRequest request) {
+	HttpSession session = request.getSession();
+	
+	int selectedBankId = Integer.valueOf((String) session.getAttribute("selectedBankId"));
+	Bank selectedBank = bankService.getBankById(selectedBankId);
+	int selectedWalletId = Integer.valueOf((String) session.getAttribute("selectedWalletId"));
+	Wallet selectedWallet = walletService.getWalletById(selectedWalletId);
+	
+	double amountReceive = Double.valueOf(transferAmount);
+	double amountRemit = amountReceive / selectedWallet.getCurrency().getCurrencyRate() * selectedBank.getCurrency().getCurrencyRate(); 
+	selectedBank.setBalance(selectedBank.getBalance()-amountRemit);
+	selectedWallet.setWalletBalance(selectedWallet.getWalletBalance()+amountReceive);
+	bankService.updateAccount(selectedBank);
+	walletService.updateWallet(selectedWallet);
+	session.removeAttribute("selectedBankId");
+	session.removeAttribute("selectedWalletId");
+	return "redirect:/" + username + "/wallet";
     }
 }
--- project/src/main/java/com/fdm/project/repo/BankRepo.java
+++ project/src/main/java/com/fdm/project/repo/BankRepo.java
@@ -6,9 +6,12 @@ import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.fdm.project.model.Bank;
+import com.fdm.project.model.Currency;
 import com.fdm.project.model.User;
 
 public interface BankRepo extends JpaRepository<Bank, Integer>{
     List<Bank> getByUser(User user);
     Optional<List<Bank>> findByUser(User user);
+    
+    List<Bank> getByUserAndCurrency(User user, Currency currency);
 }
