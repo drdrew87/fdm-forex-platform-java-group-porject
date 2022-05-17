c7f272ad820b2ece80dba06aae7013c5975023d7
amos yau <amos.yau@fdmgroup.com>
2022-05-17 11:46:48 +0800

    Resolve SpotOrderForexRepo conflicts

--- project/src/main/java/com/fdm/project/repo/SpotOrderForexRepo.java
+++ project/src/main/java/com/fdm/project/repo/SpotOrderForexRepo.java
@@ -11,61 +11,65 @@ import com.fdm.project.model.Currency;
 import com.fdm.project.model.SpotOrderForex;
 import com.fdm.project.model.User;
 
-public interface SpotOrderForexRepo extends JpaRepository<SpotOrderForex, Integer>{
+public interface SpotOrderForexRepo extends JpaRepository<SpotOrderForex, Integer> {
     List<SpotOrderForex> getByUser(User user);
-    Optional<List<SpotOrderForex>> findByUser(User user);
-	Optional<List<SpotOrderForex>> findByIsActive(Boolean statement);
-	Optional<List<SpotOrderForex>>findByIsActiveAndSellAmount(boolean isActive, double sellAmount);
-	Optional<List<SpotOrderForex>>findByIsActiveAndSellAmountIsNot(boolean isActive, double sellAmount);
 
-	@Query("SELECT sof FROM SpotOrderForex sof "
-    	+ "WHERE isActive = true "
-    	+ "AND buyAmount > 0 "
-    	+ "AND sellAmount > 0 "
-	+ "AND expiryDate >= current_date "
-    	+ "AND user <> :currentUser "
-    	+ "ORDER BY sellAmount/buyAmount DESC, expiryDate ASC, orderId ASC")
-    List<SpotOrderForex> getLimitOrdersListNotByUser(@Param("currentUser") User currentUser);
-    
+    Optional<List<SpotOrderForex>> findByUser(User user);
+
+    Optional<List<SpotOrderForex>> findByIsActive(Boolean statement);
+
+    Optional<List<SpotOrderForex>> findByIsActiveAndSellAmount(boolean isActive, double sellAmount);
+
+    Optional<List<SpotOrderForex>> findByIsActiveAndSellAmountIsNot(boolean isActive, double sellAmount);
+
     @Query("SELECT sof FROM SpotOrderForex sof "
-	+ "WHERE isActive = true "
-	+ "AND buyAmount > 0 "
-	+ "AND sellAmount = 0 "
-	+ "AND expiryDate >= current_date "
-	+ "AND user <> :currentUser "
-	+ "ORDER BY expiryDate ASC, orderId ASC")
+            + "WHERE isActive = true "
+            + "AND buyAmount > 0 "
+            + "AND sellAmount > 0 "
+            + "AND expiryDate >= current_date "
+            + "AND user <> :currentUser "
+            + "ORDER BY sellAmount/buyAmount DESC, expiryDate ASC, orderId ASC")
+    List<SpotOrderForex> getLimitOrdersListNotByUser(@Param("currentUser") User currentUser);
+
+    @Query("SELECT sof FROM SpotOrderForex sof "
+            + "WHERE isActive = true "
+            + "AND buyAmount > 0 "
+            + "AND sellAmount = 0 "
+            + "AND expiryDate >= current_date "
+            + "AND user <> :currentUser "
+            + "ORDER BY expiryDate ASC, orderId ASC")
     List<SpotOrderForex> getMarketOrdersListNotByUser(@Param("currentUser") User currentUser);
 
     @Query("SELECT sof FROM SpotOrderForex sof "
-	    	+ "WHERE isActive = true "
-	    	+ "AND buyAmount > 0 "
-	    	+ "AND sellAmount > 0 "
-		+ "AND expiryDate >= current_date "
-	    	+ "AND user <> :currentUser "
-	    	+ "AND sellCurrency = :buyCurrency "
-	    	+ "AND buyCurrency = :sellCurrency "
-	    	+ "ORDER BY sellAmount/buyAmount DESC, expiryDate ASC, orderId ASC")
+            + "WHERE isActive = true "
+            + "AND buyAmount > 0 "
+            + "AND sellAmount > 0 "
+            + "AND expiryDate >= current_date "
+            + "AND user <> :currentUser "
+            + "AND sellCurrency = :buyCurrency "
+            + "AND buyCurrency = :sellCurrency "
+            + "ORDER BY sellAmount/buyAmount DESC, expiryDate ASC, orderId ASC")
     List<SpotOrderForex> getLimitOrderMatchingList(@Param("currentUser") User currentUser, @Param("buyCurrency") Currency buyCurrency, @Param("sellCurrency") Currency sellCurrency);
-    
-    
+
+
     @Query("SELECT sof FROM SpotOrderForex sof "
-	    	+ "WHERE isActive = true "
-	    	+ "AND buyAmount > 0 "
-	    	+ "AND sellAmount = 0 "
-		+ "AND expiryDate >= current_date "
-	    	+ "AND user <> :currentUser "
-	    	+ "AND sellCurrency = :buyCurrency "
-	    	+ "AND buyCurrency = :sellCurrency "
-	    	+ "ORDER BY expiryDate ASC, orderId ASC")
+            + "WHERE isActive = true "
+            + "AND buyAmount > 0 "
+            + "AND sellAmount = 0 "
+            + "AND expiryDate >= current_date "
+            + "AND user <> :currentUser "
+            + "AND sellCurrency = :buyCurrency "
+            + "AND buyCurrency = :sellCurrency "
+            + "ORDER BY expiryDate ASC, orderId ASC")
     List<SpotOrderForex> getMarketOrderMatchingList(@Param("currentUser") User currentUser, @Param("buyCurrency") Currency buyCurrency, @Param("sellCurrency") Currency sellCurrency);
-    
-    
+
+
     @Query("SELECT sof FROM SpotOrderForex sof "
-	    	+ "WHERE isActive = false "
-	    	+ "OR buyAmount = 0 "
-		+ "OR expiryDate < current_date")
+            + "WHERE isActive = false "
+            + "OR buyAmount = 0 "
+            + "OR expiryDate < current_date")
     List<SpotOrderForex> getRemovableOrderList();
-    
+
 
 }
 
